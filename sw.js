e2037cb269dc0d8dc61e7ee58aededc5c7fa54b2
みゆ <quruquru99999@gmail.com>
2026-09-17 17:27:12 +0900

    アイコン更新に伴いservice workerキャッシュをバージョンアップ
    インストール済みユーザーに新アイコンを確実に反映させるためキャッシュ名をv3に

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'pdf-highlight-lite-v2';
+const CACHE_NAME = 'pdf-highlight-lite-v3';
 const APP_SHELL = ['./', './index.html', './icon-192.png', './icon-512.png'];
 
 self.addEventListener('install', (e) => {
