d8a3f19996ce2471230ccf7111133314ee0582f9
みゆ <quruquru99999@gmail.com>
2026-09-17 23:04:35 +0900

    アプリ名を「でるとこライン」に変更
    manifest.json・タイトル・アプリ内表示名を刷新。リポジトリ名/公開URLは変更なし

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'pdf-highlight-lite-v3';
+const CACHE_NAME = 'pdf-highlight-lite-v4';
 const APP_SHELL = ['./', './index.html', './icon-192.png', './icon-512.png'];
 
 self.addEventListener('install', (e) => {
